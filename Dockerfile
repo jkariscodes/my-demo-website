8c567ad75f529ac1b43e804437013d986f3ba931
Joseph Kariuki <joehene@gmail.com>
2022-06-14 10:35:50 +0300

    Dockerized mywebsite project.

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Base image
+
+FROM python:3.8.13-slim-buster
+
+ENV PYTHONDONTWRITEBYTECODE 1
+
+ENV PYTHONUNBUFFERED 1
+
+# Working directory
+
+WORKDIR /website
+
+# Installing dependencies
+
+COPY Pipfile Pipfile.lock /website/
+
+RUN pip install pipenv && pipenv install --system
+
+# Copy project files and directories
+
+COPY . /website/
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': "database",
+        'PORT': 5432,
     }
 }
 
