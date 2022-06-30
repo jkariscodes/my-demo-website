178c2a460b4418ede81b7a49d4fb8d26f9f21c61
Joseph Kariuki <joehene@gmail.com>
2022-06-30 07:26:13 +0300

    Reconfigured heroku deployments configurations.

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -180,6 +180,7 @@ if ENVIRONMENT == 'production':
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Database URL
 db_env = dj_database_url.config(conn_max_age=600)
