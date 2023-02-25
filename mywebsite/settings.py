6669178aa4eef4dc7a459b07618e254fa7794f31
Joseph Kariuki <joehene@gmail.com>
2023-02-26 01:07:03 +0300

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -188,7 +188,7 @@ if ENVIRONMENT == "production":
     AWS_LOCATION = os.environ.get("AWS_S3_CUSTOM_DOMAIN")
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     DEFAULT_FILE_STORAGE = "mywebsite.storage_backends.MediaStorage"
-    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
     ]
