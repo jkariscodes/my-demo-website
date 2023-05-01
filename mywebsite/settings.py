2d878bddc50f7dc0b419e5b0a9f8dbe311447348
Joseph Kariuki <joehene@gmail.com>
2023-05-01 12:07:49 +0300

    Fixed issue with removing session middleware.

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    # "django.contrib.sessions.middleware.SessionMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
