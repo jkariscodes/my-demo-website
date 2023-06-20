3768f1ff20ef794a1d87ca40ffba3fb9e25449b6
Joseph Kariuki <joehene@gmail.com>
2023-06-21 01:45:04 +0300

    Removed duplicated base directory reference.

--- mywebsite/settings/prod.py
+++ mywebsite/settings/prod.py
@@ -3,9 +3,6 @@ import environ
 
 env = environ.Env(DEBUG=(bool, False))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
